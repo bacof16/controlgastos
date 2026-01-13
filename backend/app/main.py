306b7b199d20b1c510c2d60efe0a570222399782
JC <jbelmarc@gmail.com>
2026-01-13 19:34:14 -0300

    ETAPA 3.1 - Registrar router de notificaciones en main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+# Include routers
+from app.routers import notifications
+
+app.include_router(notifications.router, prefix="/api")
+
 
 @app.get("/")
 def read_root():

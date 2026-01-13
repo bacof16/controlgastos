4538f5359a16162378947e50808f2cfed90ae6c0
JC <jbelmarc@gmail.com>
2026-01-13 13:41:12 -0300

    Add Company model

---
+++ backend/app/models/company.py
@@ -0,0 +1,14 @@
+import uuid
+from datetime import datetime
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.dialects.postgresql import UUID
+from .base import Base
+
+
+class Company(Base):
+    __tablename__ = "companies"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime(timezone=True), default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False)

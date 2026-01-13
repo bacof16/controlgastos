4b121ad033c5fed557c6d1c74e45bc4298decae6
JC <jbelmarc@gmail.com>
2026-01-13 13:58:19 -0300

    Update __init__.py with new models

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -2,5 +2,7 @@ from .base import Base
 from .company import Company
 from .user import User
 from .company_user import CompanyUser
+from .recurring_template import RecurringTemplate
+from .payment import Payment
 
-__all__ = ["Base", "Company", "User", "CompanyUser"]
+__all__ = ["Base", "Company", "User", "CompanyUser", "RecurringTemplate", "Payment"]
